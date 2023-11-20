bdb89946e7ad086c0d6cae2b93f4ac37596c2502
Salmanulfaris.k <salmanulfaris4511@gmail.com>
2023-11-20 14:20:39 +0530

    navigation and architecture

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_api/core/colors/colors.dart';
+import 'package:netflix_api/presentation/main_page/screen_main_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       scaffoldBackgroundColor:backGroundColor,
+        colorScheme: ColorScheme.fromSeed(seedColor:Colors.black),
+       textTheme: const TextTheme(
+        bodyLarge:TextStyle(
+          color: Colors.white
+        ) ,
+        bodyMedium: TextStyle(
+          color: Colors.white
+        )
+       ),
+        useMaterial3: true,
+      ),
+      home:  ScreenMainPage(),
+    );
+  }
+}
+
+
+
+
+ 
