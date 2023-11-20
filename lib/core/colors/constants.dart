b16825be3a3389bca3efc2b37a0a8cbaf749ebc2
Salmanulfaris.k <salmanulfaris4511@gmail.com>
2023-11-21 02:19:01 +0530

    downloadsScreen

---
+++ lib/core/colors/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kwidth= SizedBox(width: 10,);
+const kwhite= Colors.white;
+final buttoncolorblue= Colors.blueAccent[700];
+const buttoncolorWhite = Colors.white;
+const kblackcolor= Colors.black;
+const kheight=SizedBox(height: 10,);
--- lib/presentation/main_page/screen_main_page.dart
+++ lib/presentation/main_page/screen_main_page.dart
@@ -15,7 +15,7 @@ class ScreenMainPage extends StatelessWidget {
     const ScreenNewAndHot(),
     const ScreenFastLaugh(),
     const ScreenSearch(),
-    const ScreenDownloads(),
+     ScreenDownloads(),
   ];
   @override
   Widget build(BuildContext context) {
